15977f0c23f71ace8951636443778edf83e38bd2
cwomw <mikewright367@u.boisestate.edu>
2016-06-10 00:26:36 -0600

    Create ModernArt.java

---
+++ ModernArt.java
@@ -0,0 +1,17 @@
+/**   This program will reproduce art of Piet Mondrain
+*
+*
+*
+*   @author  Michael Wright
+*
+*
+*/
+public class ModernArt
+{
+    public static void main(String[] args);
+    {
+    System.out.pl("-(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)-");
+    System.out.pl("-(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)(<>)-");
+    System.out.pl("-\u2576\u25co\u25b7\u25c1-");
+    }
+}
